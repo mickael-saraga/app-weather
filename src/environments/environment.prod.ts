844f0c2de0b12de538e53b52936d94e2f2d09e2a
mickael-saraga <mickael.m.saraga@gmail.com>
2022-01-04 15:22:20 +0100

    built app to folder docs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,19 @@
 export const environment = {
-  production: true
+  production: true,
+  OPEN_WEATHER_URL: 'https://api.openweathermap.org/data/2.5/',
+  API_ID: '5a4b2d457ecbef9eb2a71e480b947604',
+  COUNTRY_ISO_CODE: 'FR',
+  lang: 'fr',
+  country: 'France',
+  units: 'metric',
+  storage_zipcodes_key: 'zipcodes',
+  paths: {
+    forecast: 'forecast'
+  },
+  queryParams: {
+    zipcode: 'zipcode',
+    name: 'name',
+    latitude: 'lat',
+    longoitude: 'lon',
+  }
 };
