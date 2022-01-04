8d41ab5c6181c629cd2e5756e21d0d08abb6be8a
mickael-saraga <mickael.m.saraga@gmail.com>
2022-01-04 13:37:47 +0100

    added button for another navigation possibility to week forecast

--- src/app/modules/location/location.component.ts
+++ src/app/modules/location/location.component.ts
@@ -1,4 +1,6 @@
-import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { Router } from "@angular/router";
+import { environment } from "../../../environments/environment";
 import { Location } from "../../models/location.model";
 
 @Component({
@@ -10,9 +12,23 @@ export class LocationComponent {
   @Input() location: Location | undefined;
   @Output() removeLocationAsked = new EventEmitter<number>();
 
+  constructor(private router: Router) { }
+
   removeLocation(locationZipCode: number) {
     event?.stopPropagation();
     this.removeLocationAsked.emit(locationZipCode);
+  }
+  
+  openLocationWeek(location: Location) {
+    event?.stopPropagation();
+    if (location && location.zipCode) {
+      const coordinates = {
+        name: location.name,
+        lat: location.latitude,
+        lon: location.longitude
+      };
+      this.router.navigate([environment.paths.forecast, location.zipCode], { queryParams: coordinates });
+    }
   }
 
 }
